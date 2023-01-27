8272fd829eddea1c68d44f76280287f7cb54df9a
crazynavi <117359800+TrustedSeven@users.noreply.github.com>
2023-01-27 11:30:33 -0600

    task added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+import "@fortawesome/free-solid-svg-icons"
+import "@fortawesome/react-fontawesome"
+
+import Task from './container/task';
+import Project from './container/project';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Time Tracking
-        </a>
-      </header>
-    </div>
+    <Task/>
   );
 }
 
