dc27a87ac977d0ac5e3e4cfecb52bb578c95bd5f
crazynavi <117359800+TrustedSeven@users.noreply.github.com>
2023-01-28 10:16:02 -0600

    dropdown created

--- src/container/task/index.js
+++ src/container/task/index.js
@@ -1,18 +1,15 @@
 import React from "react";
 import {
-  MDBBadge,
-  MDBBtn,
   MDBCard,
   MDBCardBody,
   MDBCol,
   MDBContainer,
-  MDBIcon,
   MDBRow,
   MDBTable,
   MDBTableBody,
   MDBTableHead,
-  MDBTooltip,
 } from "mdb-react-ui-kit";
+import Dropdown from "react-bootstrap/Dropdown";
 
 import "@fortawesome/free-solid-svg-icons";
 import "@fortawesome/react-fontawesome";
@@ -21,13 +18,26 @@ import "./index.css";
 
 export default function Task() {
   return (
-    <section className="gradient-custom-2 mt-5 vh-100">
-      <MDBContainer className="py-5 mt-5 h-100">
+    <section className="gradient-custom-2 vh-100">
+      <div className="dropdownmenu pt-5">
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            View Mode
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Admin</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Project</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Task</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+      <MDBContainer className="py-5 h-100">
         <MDBRow className="d-flex justify-content-center align-items-center">
           <MDBCol md="12" xl="10">
             <MDBCard className="mask-custom">
               <MDBCardBody className="p-4 text-white bg-info">
-                <div className="text-center mt-5 pt-3 pb-2">
+                <div className="text-center mt-5 pb-2">
                   <img
                     src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-todo-list/check1.webp"
                     alt="Check"
@@ -69,12 +79,7 @@ export default function Task() {
                         <span>5 hours</span>
                       </td>
                       <td className="align-middle">
-                        <MDBBtn className="me-1" color="success">
-                          Success
-                        </MDBBtn>
-                        <MDBBtn className="me-1" color="warning">
-                          Warning
-                        </MDBBtn>
+                        <button className="custombutton">Edit</button>
                       </td>
                     </tr>
                     <tr className="fw-normal">
@@ -99,12 +104,7 @@ export default function Task() {
                         <span>8 hours</span>
                       </td>
                       <td className="align-middle">
-                        <MDBBtn className="me-1" color="success">
-                          Success
-                        </MDBBtn>
-                        <MDBBtn className="me-1" color="warning">
-                          Warning
-                        </MDBBtn>
+                        <button className="custombutton">Edit</button>
                       </td>
                     </tr>
                     <tr className="fw-normal">
@@ -129,12 +129,7 @@ export default function Task() {
                         <span>7 hours</span>
                       </td>
                       <td className="align-middle">
-                        <MDBBtn className="me-1" color="success">
-                          Success
-                        </MDBBtn>
-                        <MDBBtn className="me-1" color="warning">
-                          Warning
-                        </MDBBtn>
+                        <button className="custombutton">Edit</button>
                       </td>
                     </tr>
                   </MDBTableBody>
